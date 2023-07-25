f964fabe7e8eefa4bf67f23d5c1d5cfbfb4ae918
wirix <88577048+wirix@users.noreply.github.com>
2023-07-25 11:10:53 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { ReactEx } from './react-example/ReactExample';
 
-function App() {
+const App = () => {
+  const [isOpened, setIsOpened] = useState(false);
+  console.log('APP')
+  const [inputState, setInputState] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div onClick={() => setIsOpened(!isOpened)}>{isOpened ? 'закрыть' : 'открыть'}</div>
+      {isOpened && <ReactEx setInputState={setInputState} inputState={inputState} />}
     </div>
   );
 }
---
+++ src/i18n/index.ts
@@ -0,0 +1,9 @@
+export {
+	I18N,
+	I18NProvider,
+	useI18N,
+	useTranslate,
+	TaggedText,
+	pluralizeEn,
+	pluralizeRu,
+} from "./lib";
